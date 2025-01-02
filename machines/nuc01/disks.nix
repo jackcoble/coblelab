40971cda3f1c92ee0d3ee10d6157ac980bc6f871
Jack Coble <jack@coble.uk>
2025-01-02 19:37:24 +0000

    feat: btrfs disko config

--- machines/nuc01/disks.nix
+++ machines/nuc01/disks.nix
@@ -1,33 +1,77 @@
 {
   disko.devices = {
     disk = {
-      sda = {
+      main = {
         type = "disk";
         device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
+            # Boot partition
             ESP = {
+              label = "boot";
+              name = "ESP";
               size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = ["defaults"];
+                mountOptions = [
+                  "defaults"
+                  "umask=0077"
+                ];
               };
             };
 
+            # LUKS partition
             luks = {
               size = "100%";
+              label = "luks";
               content = {
                 type = "luks";
-                name = "crypted";
-                settings.allowDiscards = true;
+                name = "cryptroot";
+                extraOpenArgs = [
+                  "--allow-discards"
+                  "--perf-no_read_workqueue"
+                  "--perf-no_write_workqueue"
+                ];
+
+                # BTRFS filesystem
                 content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/nix";
+                  type = "btrfs";
+                  extraArgs = ["-L" "nixos" "-f"];
+                  subvolumes = {
+                    "@root" = {
+                      mountpoint = "/";
+                      mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+                    };
+
+                    "@home" = {
+                      mountpoint = "/home";
+                      mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                    };
+
+                    "@nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                    };
+
+                    "@persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+                    };
+
+                    "@log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                    };
+
+                    "@swap" = {
+                      mountpoint = "/swap";
+                      swap.swapfile.size = "32G";
+                    };
+                  };
                 };
               };
             };
@@ -35,16 +79,8 @@
         };
       };
     };
-
-    nodev = {
-      "/" = {
-        fsType = "tmpfs";
-        mountOptions = [
-          "size=2G"
-          "defaults"
-          "mode=755"
-        ];
-      };
-    };
   };
+
+  fileSystems."/persist".neededForBoot = true;
+  fileSystems."/var/log".neededForBoot = true;
 }
