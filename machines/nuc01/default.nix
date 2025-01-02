aa051e520509f39422bdb859c444a5c4214051a4
Jack Coble <jack@coble.uk>
2025-01-02 21:15:37 +0000

    feat: create a nixos module for configuration i want on all machines

--- machines/nuc01/default.nix
+++ machines/nuc01/default.nix
@@ -1,33 +1,13 @@
 {pkgs, ...}: {
   imports = [
+    ../../modules/nixos
     ./hardware-configuration.nix
     ./disks.nix
   ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   # Networking.
   networking.networkmanager.enable = true;
   networking.hostName = "nuc01";
-
-  # Set Time Zone.
-  time.timeZone = "Europe/London";
-
-  # Configure console keymap.
-  console.keyMap = "uk";
-
-  # Allow unfree packages.
-  nixpkgs.config.allowUnfree = true;
-
-  # Experimental features.
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # List packages installed in system profile.
-  environment.systemPackages = with pkgs; [
-    git
-  ];
 
   # Enable OpenSSH.
   services.openssh.enable = true;
