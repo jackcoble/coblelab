7cb4782f6d7d7c91109389cc475f3ce43e668e29
Jack Coble <jack@coble.uk>
2025-01-03 23:29:06 +0000

    refactor: remove root volume clearing commands from initrd

--- modules/nixos/disks.nix
+++ modules/nixos/disks.nix
@@ -140,25 +140,6 @@ in {
 
       fileSystems."/persist".neededForBoot = true;
       fileSystems."/var/log".neededForBoot = true;
-
-      # Clear the root volume on every boot
-      boot.initrd.postDeviceCommands = lib.mkAfter ''
-        set -x
-        exec 1>>/tmp/initrd.log 2>&1
-
-        echo "Attempting to clear the root volume..."
-
-        mkdir -p /mnt
-        mount -t btrfs /dev/mapper/crypted /mnt
-        btrfs subvolume delete /mnt/@
-        btrfs subvolume snapshot /mnt/root-blank /mnt/@
-        umount /mnt
-
-        echo "Root volume cleared successfully."
-
-        cp /tmp/initrd.log /run/initrd.log
-        set +x
-      '';
     })
   ];
 }
