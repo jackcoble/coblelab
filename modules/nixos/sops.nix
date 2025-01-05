d3ab9ae84788dfa092096ff1dec660ca1969e5fa
Jack Coble <jack@coble.uk>
2025-01-05 14:32:36 +0000

    fix: decryption of sops-nix secrets

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -2,7 +2,11 @@
 This SOPS module takes care of loading secrets, decrypting them using the Host SSH Key
 */
 {...}: {
-  sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  sops.age.sshKeyPaths = [
+    # on first activation the symlink to /etc isn't there yet
+    "/persist/etc/ssh/ssh_host_ed25519_key"
+    "/etc/ssh/ssh_host_ed25519_key"
+  ];
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
 
   # Declare secrets
