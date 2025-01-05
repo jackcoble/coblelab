e24cae78e9f90e82d92469033cb97eaf28c554c9
Jack Coble <jack@coble.uk>
2025-01-05 18:01:42 +0000

    fix: invalid ssh restic host

--- modules/nixos/backups.nix
+++ modules/nixos/backups.nix
@@ -13,7 +13,7 @@ This backups module uses Restic to backup
       initialize = true;
       passwordFile = config.sops.secrets.restic-password.path; # Encryption password
       extraOptions = [
-        "sftp.command='ssh u441231@u441231.your-storagebox -p 23 -i /etc/ssh/ssh_host_ed25519_key -s sftp'"
+        "sftp.command='ssh u441231@u441231.your-storagebox.de -p 23 -i /etc/ssh/ssh_host_ed25519_key -s sftp'"
       ];
 
       # What do we want to back up?
