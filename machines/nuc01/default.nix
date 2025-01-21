c2df66f5facd218361bd46e6575b5d730208a01b
Jack Coble <jack@coble.uk>
2025-01-21 17:00:25 +0000

    feat: enable podman for containers

--- machines/nuc01/default.nix
+++ machines/nuc01/default.nix
@@ -34,6 +34,19 @@
   # Impermanence.
   coblelab.impermanence.enable = true;
 
+  # Podman (Containers)
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   # Networking.
   networking.networkmanager.enable = true;
   networking.hostName = "nuc01";
