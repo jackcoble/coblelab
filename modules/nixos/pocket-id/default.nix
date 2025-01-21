1a23deaf82ce6491546f30825b15aeaea81b2fd6
Jack Coble <jack@coble.uk>
2025-01-21 17:49:31 +0000

    feat: turn pocket-id into a module

--- modules/nixos/pocket-id/default.nix
+++ modules/nixos/pocket-id/default.nix
@@ -1,22 +1,45 @@
-{...}: {
-  virtualisation.oci-containers.containers."pocket-id" = {
-    image = "ghcr.io/stonith404/pocket-id";
+{
+  options,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.coblelab.pocket-id;
+in {
+  # Configuration options
+  options.coblelab.pocket-id = {
+    enable = lib.mkEnableOption "Enable Pocket ID";
 
-    volumes = [
-      "/zstorage/docker/pocket-id:/app/backend/data"
-    ];
+    port = lib.mkOption {
+      type = lib.types.int;
+      default = 11000;
+      description = "The port on which Pocket ID will listen";
+    };
 
-    ports = [
-      "127.0.0.1:11000:8081"
-    ];
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = /zstorage/docker/pocket-id;
+      description = "The directory where Pocket ID will store its data";
+    };
 
-    environment = {
-      # See the README for more information: https://github.com/stonith404/pocket-id?tab=readme-ov-file#environment-variables
-      PUBLIC_APP_URL = "https://auth.coblelabs.net";
-      TRUST_PROXY = "true";
-      PUID = "1000";
-      PGID = "1000";
-      CADDY_PORT = "8081";
+    # See the README for more information: https://github.com/stonith404/pocket-id?tab=readme-ov-file#environment-variables
+    environment = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {};
+      description = "Environment variables to pass to the Pocket ID container";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    virtualisation.oci-containers.containers."pocket-id" = {
+      image = "ghcr.io/stonith404/pocket-id";
+      environment = cfg.environment;
+      volumes = [
+        "${cfg.dataDir}:/app/backend/data"
+      ];
+      ports = [
+        "127.0.0.1:${cfg.port}:8081"
+      ];
     };
   };
 }
