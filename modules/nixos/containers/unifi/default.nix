cb57a9eca936892f7dc3f31ace366d1ddceb6fd6
Jack Coble <jack@coble.uk>
2025-02-26 23:06:59 +0000

    refactor: persistance and backups depend on other config options

--- modules/nixos/containers/unifi/default.nix
+++ modules/nixos/containers/unifi/default.nix
@@ -15,36 +15,45 @@ in {
     coblelab.containers.unifi.enable = lib.mkEnableOption "UniFi Controller";
   };
 
-  config = lib.mkIf config.coblelab.containers.unifi.enable {
-    environment.systemPackages = [pkgs.podman];
+  config = lib.mkMerge [
+    (lib.mkIf config.coblelab.containers.unifi.enable {
+      environment.systemPackages = [pkgs.podman];
 
-    systemd.tmpfiles.rules = [
-      "d ${dataDir} 0755 unifi unifi -"
-    ];
+      systemd.tmpfiles.rules = [
+        "d ${dataDir} 0755 unifi unifi -"
+      ];
 
-    users.groups.unifi = {};
-    users.users.unifi = {
-      isSystemUser = true;
-      group = "unifi";
-    };
-
-    virtualisation.oci-containers.containers.${containerName} = {
-      image = image;
-      autoStart = true;
-      ports = ["8443:8443" "8080:8080" "3478:3478/udp" "10001:10001/udp"];
-      volumes = ["${dataDir}:/unifi"];
-      environment = {
-        TZ = "UTC";
+      users.groups.unifi = {};
+      users.users.unifi = {
+        isSystemUser = true;
+        group = "unifi";
       };
-      extraOptions = ["--network=host"];
-    };
 
-    networking.firewall.allowedTCPPorts = [8443 8080];
-    networking.firewall.allowedUDPPorts = [3478 10001];
+      virtualisation.oci-containers.containers.${containerName} = {
+        image = image;
+        autoStart = true;
+        ports = ["8443:8443" "8080:8080" "3478:3478/udp" "10001:10001/udp"];
+        volumes = ["${dataDir}:/unifi"];
+        environment = {
+          TZ = "UTC";
+        };
+        extraOptions = ["--network=host"];
+      };
 
-    # Persist the data directory
-    environment.persistence."${config.coblelab.impermanence.persistDirectory}" = {
-      directories = [dataDir];
-    };
-  };
+      networking.firewall.allowedTCPPorts = [8443 8080];
+      networking.firewall.allowedUDPPorts = [3478 10001];
+    })
+
+    # If Persistance mode is enabled, ensure the data directory is persisted
+    (lib.mkIf config.coblelab.impermanence.enable {
+      environment.persistence."${config.coblelab.impermanence.persistDirectory}" = {
+        directories = [dataDir];
+      };
+    })
+
+    # If the backups module is enabled, ensure the "auto backups" from the controller are backed up
+    (lib.mkIf config.coblelab.backups.enable {
+      services.restic.backups.hetzner-storage-box.paths = ["${dataDir}/data/backup/autobackup"];
+    })
+  ];
 }
