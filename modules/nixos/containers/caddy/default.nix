b18e0d44ab479434ab124726707d6afaae9b8df1
Jack Coble <jack@coble.uk>
2025-02-28 14:50:46 +0000

    feat: caddy service

--- modules/nixos/containers/caddy/default.nix
+++ modules/nixos/containers/caddy/default.nix
@@ -2,61 +2,42 @@
   options,
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.coblelab.containers.caddy;
 in {
   options.coblelab.containers.caddy = {
     enable = lib.mkEnableOption "Caddy";
-
-    dataDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/var/lib/caddy";
-      description = "The directory where Caddy will store its data";
-    };
   };
 
   config = lib.mkIf cfg.enable {
-    # Create user and group for Caddy
-    users.groups.caddy = {};
-    users.users.caddy = {
-      isSystemUser = true;
-      group = "caddy";
-    };
+    # Allow network access when building
+    nix.settings.sandbox = false;
 
-    # Create the data directory
-    systemd.tmpfiles.rules = [
-      "f ${cfg.dataDir}/Caddyfile 0755 caddy caddy -"
-      "d ${cfg.dataDir}/data 0755 caddy caddy -"
-      "d ${cfg.dataDir}/config 0755 caddy caddy -"
+    # Expose ports through Firewall
+    networking.firewall.allowedTCPPorts = [
+      80
+      443
     ];
 
-    # Create the Caddy container
-    virtualisation.oci-containers.containers."caddy" = {
-      image = "ghcr.io/caddybuilds/caddy-cloudflare:latest";
-      volumes = [
-        "${cfg.dataDir}/Caddyfile:/etc/caddy/Caddyfile"
-        "${cfg.dataDir}/data:/data"
-        "${cfg.dataDir}/config:/config"
-      ];
-      ports = [
-        "80:80"
-        "443:443"
-      ];
+    # Caddy service
+    services.caddy = {
+      enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = ["github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e"];
+        hash = "sha256-WGV/Ve7hbVry5ugSmTYWDihoC9i+D3Ct15UKgdpYc9U=";
+      };
     };
 
-    # Open 80 + 443 TCP ports
-    networking.firewall.allowedTCPPorts = [80 443];
-
-    # Set kernel parameters to allow 80 to be the lowest unprivileged port
-    boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
-
-    # If Persistance mode is enabled, ensure the data directory is persisted
-    environment.persistence."${config.coblelab.impermanence.persistDirectory}" = {
-      directories = [cfg.dataDir];
+    systemd.services.caddy = {
+      serviceConfig = {
+        # Required to use ports < 1024
+        AmbientCapabilities = "cap_net_bind_service";
+        CapabilityBoundingSet = "cap_net_bind_service";
+        EnvironmentFile = config.sops.secrets.cloudflare-api-key.path;
+        TimeoutStartSec = "5m";
+      };
     };
-
-    # Restic
-    services.restic.backups.hetzner-storage-box.paths = [cfg.dataDir];
   };
 }
